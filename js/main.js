a593a0c661e61ce9e79ffb16793d4a6d8f2a64db
moustafa <moustafasaad954@gmail.com>
2022-06-25 04:42:40 +0200

    finish the product

---
+++ js/main.js
@@ -0,0 +1,70 @@
+// https://api.adviceslip.com/advice
+
+// connect to api by xmlrequest
+// let myreq = new XMLHttpRequest();
+// myreq.open("GET", "https://api.adviceslip.com/advice");
+// myreq.send();
+// myreq.onload = function () {
+//   if (this.readyState === 4 && this.status === 200) {
+//     console.log(myreq.responseText);
+//   }
+// };
+
+// connect to api by promise
+// let mypromReq = (api) =>
+//   new Promise((res, rej) => {
+//     let myreq = new XMLHttpRequest();
+//     myreq.open("GET", api);
+//     myreq.send();
+//     myreq.onload = function () {
+//       if (this.readyState === 4 && this.status === 200) {
+//         res(JSON.parse(myreq.responseText));
+//       } else {
+//         rej("access denied");
+//       }
+//     };
+//   });
+
+// mypromReq("https://api.adviceslip.com/advice").then((res) => {
+//   console.log(res);
+// });
+
+// connect to api by fetch
+// let prom = fetch("https://api.adviceslip.com/advice");
+// prom
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+
+// connect to api by async and fetch
+async function getData(api) {
+  let myprom = await fetch(api);
+  let apiData = await myprom.json();
+  let advice = apiData["slip"]["advice"];
+  let id = apiData["slip"]["id"];
+  // get advice q and id span
+  let advQ = document.querySelector(".adviceCont blockquote q");
+  let advSpanId = document.querySelector(".adviceCont h2 span");
+  advQ.innerHTML = advice;
+  advSpanId.innerHTML = id;
+}
+
+// get advice every 2 second
+// getData("https://api.adviceslip.com/advice");
+// setInterval(() => {
+//   getData("https://api.adviceslip.com/advice");
+// }, 2000);
+
+let dice = document.querySelector(".adviceCont .dice-cont");
+dice.addEventListener("click", () => {
+  dice.style.backgroundColor = "red";
+  setTimeout(() => {
+    getData("https://api.adviceslip.com/advice");
+    setTimeout(() => {
+      dice.style.backgroundColor = "var(--Neon-Grean)";
+    }, 500);
+  }, 500);
+});
